0e1ac53a08079e6700a6fc233e78b387849d5683
onimojito <mailbox140@gmail.com>
2019-10-27 20:58:31 +0300

    Homework 2

---
+++ 2/homework_overflow_char.c
@@ -0,0 +1,13 @@
+// Author: Alexander G.
+// git commit
+// git push
+// Покинуть помещение
+
+#include <stdio.h>
+
+int main() {
+  char x = (char)126;
+  printf("x character: %c\n", ++x);
+  printf("x digit: %d\n", ++x);
+  return 0;
+}
